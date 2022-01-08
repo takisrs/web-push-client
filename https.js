2e81d64525baa08ebade1b637fdf6906d7cd7326
Panagiotis Pantazopoulos <takispadaz@gmail.com>
2022-01-08 03:18:20 +0200

    Revert "add support for https"
    This reverts commit aa6c8dd2732cf0fa315781c2d94b4d255de16229.

--- https.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
- 
-module.exports = {
-    cert: fs.readFileSync(__dirname + "/cert.pem"),
-    key: fs.readFileSync(__dirname + "/key.pem"),
-    passphrase: "notifications"
-};
--- init-server.js
+++ init-server.js
@@ -8,6 +8,5 @@ const params = {
   file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
   logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-  https: './https.js'
 };
 liveServer.start(params);
